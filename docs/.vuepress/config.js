70341d7c828a46171f9b35276b5e441a63d99bd5
lynne.liu <lynne.liu@xiaoying.com>
2020-09-25 18:56:12 +0800

    http响应码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,8 @@ module.exports = {
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
-          {path:'/sept/url-action', title: '第一篇：从浏览器地址栏输入url开始'}
+          { path:'/sept/url-action', title: '第一篇：从浏览器地址栏输入url开始' },
+          { path:'/sept/deep-clone', title: '第二篇：走进lodash深拷贝' }
         ]
       },
     ]
--- docs/README.md
+++ docs/README.md
@@ -3,4 +3,7 @@ home: false
 ---
 # 目录
 
-* [从浏览器地址栏输入url开始](/sept/day-one)
+* [从浏览器地址栏输入url开始](/sept/url-action)
+* [lodash深拷贝源码解读](/sept/deep-clone)
+* [走进promise](/sept/promise)
+* [js原型链](/sept/prototype-line)
